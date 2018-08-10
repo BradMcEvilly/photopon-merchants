befb813d41ac7788c25051888112ed12deca2657
stephan.com <stephan@stephan.com>
2018-08-09 20:26:05 -0400

    warn on deploy with ASCII ART
    because I’m a dork like that.
    
    ```
                    _ _              _      __
      __ _ ___  ___(_|_)   __ _ _ __| |_   / _| ___  _ __ _____   _____ _ __
     / _` / __|/ __| | |  / _` | '__| __| | |_ / _ \| '__/ _ \ \ / / _ \ '__|
    | (_| \__ \ (__| | | | (_| | |  | |_  |  _| (_) | | |  __/\ V /  __/ |
     \__,_|___/\___|_|_|  \__,_|_|   \__| |_|  \___/|_|  \___| \_/ \___|_|
    ```

--- grunt/exec.js
+++ grunt/exec.js
@@ -1,8 +1,12 @@
+asciify = require('asciify')
 module.exports = {
 
     
     uploadpackage: {
         cmd: function () {
+            asciify('deploy!', { font: 'colossal', color: 'red' }, function(err, res){ console.log(res) });
+            asciify(global.filename, { font: 'drpepper', color: 'blue'}, function(err, res){ console.log(res) });
+            console.log('deploying file ' + global.filename + ' to production');
             //return "scp -rv " + global.filename + " bradmcevilly@photopon.co:/home/bradmcevilly/releases/";
             return "rsync -avzhe ssh --progress " + global.filename + " bradmcevilly@photopon.co:/home/bradmcevilly/releases/";
         }
